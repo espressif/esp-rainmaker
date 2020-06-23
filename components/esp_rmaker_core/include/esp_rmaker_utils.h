4796f987b7292d718085f2b54746266f3e1c1dff
Chirag Atal <chirag.atal@espressif.com>
2020-06-23 17:32:19 +0530

    esp_rmaker_time_sync: Expose time_sync APIs.

--- components/esp_rmaker_core/include/esp_rmaker_utils.h
+++ components/esp_rmaker_core/include/esp_rmaker_utils.h
@@ -15,6 +15,11 @@
 #include <stdint.h>
 #include <esp_err.h>
 
+typedef struct esp_rmaker_time_config {
+    /** If not specified, then 'CONFIG_ESP_RMAKER_SNTP_SERVER_NAME' is used as the SNTP server. */
+    char *sntp_server_name;
+} esp_rmaker_time_config_t;
+
 /** Reboot the chip after a delay
  *
  * This API just starts an esp_timer and executes a reboot from that.
@@ -27,3 +32,35 @@
  * @return error on failure
  */
 esp_err_t esp_rmaker_reboot(uint8_t seconds);
+
+/** Initialize time synchronization
+ *
+ * This API initializes SNTP for time synchronization.
+ *
+ * @param[in] config Configuration to be used for SNTP time synchronization. The default configuration is used if NULL is passed.
+ *
+ * @return ESP_OK on success
+ * @return error on failure
+ */
+esp_err_t esp_rmaker_time_sync_init(esp_rmaker_time_config_t *config);
+
+/** Check if current time is updated
+ *
+ * This API checks if the current system time is updated against the reference time of 1-Jan-2019.
+ *
+ * @return true if time is updated
+ * @return false if time is not updated
+ */
+bool esp_rmaker_time_check(void);
+
+/** Wait for time synchronization
+ *
+ * This API waits for the system time to be updated against the reference time of 1-Jan-2019.
+ * This is a blocking call.
+ *
+ * @param[in] ticks_to_wait Number of ticks to wait for time synchronization. Accepted values: 0 to portMAX_DELAY.
+ *
+ * @return ESP_OK on success
+ * @return error on failure
+ */
+esp_err_t esp_rmaker_time_wait_for_sync(uint32_t ticks_to_wait);
