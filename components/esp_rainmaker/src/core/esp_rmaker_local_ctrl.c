592addd0e8b5012defdc0cee178a2674e902a38a
Piyush Shah <piyush.shah@espressif.com>
2020-12-03 22:38:29 +0530

    esp_rmaker_local_ctrl: Wait for Wi-Fi provisioning to finish before starting local control
    Since only a single protocomm_httpd instance is allowed at a time, starting local
    control before soft AP provisioning ends causes errors. So now, we wait for
    provisioning to finish before starting local control.

--- components/esp_rainmaker/src/core/esp_rmaker_local_ctrl.c
+++ components/esp_rainmaker/src/core/esp_rmaker_local_ctrl.c
@@ -14,7 +14,9 @@
 
 #include <stdlib.h>
 #include <esp_log.h>
+#include <esp_event.h>
 #include <esp_local_ctrl.h>
+#include <wifi_provisioning/manager.h>
 #include <esp_rmaker_internal.h>
 #include <esp_https_server.h>
 #include <mdns.h>
@@ -37,6 +39,8 @@ enum property_flags {
     PROP_FLAG_READONLY = (1 << 0)
 };
 
+static char *g_serv_name;
+static bool wait_for_wifi_prov;
 /********* Handler functions for responding to control requests / commands *********/
 
 static esp_err_t get_property_values(size_t props_count,
@@ -119,8 +123,12 @@ static esp_err_t set_property_values(size_t props_count,
     return ret;
 }
 
-esp_err_t esp_rmaker_start_local_ctrl_service(const char *serv_name)
+static esp_err_t __esp_rmaker_start_local_ctrl_service(const char *serv_name)
 {
+    if (!serv_name) {
+        ESP_LOGE(TAG, "Service name cannot be empty.");
+        return ESP_ERR_INVALID_ARG;
+    }
     ESP_LOGI(TAG, "Starting ESP Local control with HTTP Transport.");
     /* Set the configuration */
     static httpd_ssl_config_t https_conf = HTTPD_SSL_CONFIG_DEFAULT();
@@ -175,4 +183,61 @@ esp_err_t esp_rmaker_start_local_ctrl_service(const char *serv_name)
     ESP_ERROR_CHECK(esp_local_ctrl_add_property(&node_config));
     ESP_ERROR_CHECK(esp_local_ctrl_add_property(&node_params));
     return ESP_OK;
+}
+
+static void esp_rmaker_local_ctrl_prov_event_handler(void* arg, esp_event_base_t event_base,
+                          int event_id, void* event_data)
+{
+    ESP_LOGI(TAG, "Event %d", event_id);
+    if (event_base == WIFI_PROV_EVENT) {
+        switch (event_id) {
+            case WIFI_PROV_START:
+                wait_for_wifi_prov = true;
+                break;
+            case WIFI_PROV_DEINIT:
+                if (wait_for_wifi_prov == true) {
+                    wait_for_wifi_prov = false;
+                    if (g_serv_name) {
+                        __esp_rmaker_start_local_ctrl_service(g_serv_name);
+                        free(g_serv_name);
+                        g_serv_name = NULL;
+                    }
+                }
+                esp_event_handler_unregister(WIFI_PROV_EVENT, WIFI_PROV_START, &esp_rmaker_local_ctrl_prov_event_handler);
+                esp_event_handler_unregister(WIFI_PROV_EVENT, WIFI_PROV_DEINIT, &esp_rmaker_local_ctrl_prov_event_handler);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+esp_err_t esp_rmaker_init_local_ctrl_service(void)
+{
+    /* ESP Local Control uses protocomm_httpd, which is also used by SoftAP Provisioning.
+     * If local control is started before provisioning ends, it fails because only one protocomm_httpd
+     * instance is allowed at a time.
+     * So, we check for the WIFI_PROV_START event, and if received, wait for the WIFI_PROV_DEINIT event
+     * before starting local control.
+     * This would not be required in case of BLE Provisioning, but this code has no easy way of knowing
+     * what provisioning transport is being used and hence this logic will come into picture for both,
+     * SoftAP and BLE provisioning.
+     */
+    esp_event_handler_register(WIFI_PROV_EVENT, WIFI_PROV_START, &esp_rmaker_local_ctrl_prov_event_handler, NULL);
+    esp_event_handler_register(WIFI_PROV_EVENT, WIFI_PROV_DEINIT, &esp_rmaker_local_ctrl_prov_event_handler, NULL);
+    return ESP_OK;
+}
+
+esp_err_t esp_rmaker_start_local_ctrl_service(const char *serv_name)
+{
+    if (!wait_for_wifi_prov) {
+        return __esp_rmaker_start_local_ctrl_service(serv_name);
+    }
+
+    ESP_LOGI(TAG, "Waiting for Wi-Fi provisioning to finish.");
+    g_serv_name = strdup(serv_name);
+    if (g_serv_name) {
+        return ESP_OK;
+    }
+    return ESP_ERR_NO_MEM;
 }
