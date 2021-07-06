f1b92c293fea7bb46393ffc4f084c4a5fcb20ee6
Piyush Shah <piyush.shah@espressif.com>
2021-07-06 16:19:17 +0530

    Minor fixes
    - Compilation issues were seen on some hosts after insights components were included.
    - app_wifi: CMake file had esp_rainmaker as a dependency even if it was no dependent on it.
    - WS2812 could not be used for ESP32 due to Kconfig logic

--- components/esp_rainmaker/src/ota/esp_rmaker_ota.c
+++ components/esp_rainmaker/src/ota/esp_rmaker_ota.c
@@ -30,7 +30,7 @@ static const char *TAG = "esp_rmaker_ota";
 #define OTA_REBOOT_TIMER_SEC    10
 #define DEF_HTTP_BUFFER_SIZE    1024
 
-extern const char esp_rmaker_ota_def_cert[] asm("_binary_ota_server_crt_start");
+extern const char esp_rmaker_ota_def_cert[] asm("_binary_rmaker_ota_server_crt_start");
 const char *ESP_RMAKER_OTA_DEFAULT_SERVER_CERT = esp_rmaker_ota_def_cert;
 char *esp_rmaker_ota_status_to_string(ota_status_t status)
 {
