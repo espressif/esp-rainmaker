80eec80015e9e65eb16f04d532f7c65a5194b43a
sanket.wadekar <sanket.wadekar@espressif.com>
2022-10-11 20:41:26 +0530

    Fixed OTA Race condition
    Signed-off-by: sanket.wadekar <sanket.wadekar@espressif.com>

--- components/esp_rainmaker/src/ota/esp_rmaker_ota_internal.h
+++ components/esp_rainmaker/src/ota/esp_rmaker_ota_internal.h
@@ -21,6 +21,7 @@
 #define RMAKER_OTA_NVS_NAMESPACE            "rmaker_ota"
 #define RMAKER_OTA_JOB_ID_NVS_NAME          "rmaker_ota_id"
 #define RMAKER_OTA_UPDATE_FLAG_NVS_NAME     "ota_update"
+#define RMAKER_OTA_FETCH_DELAY              5
 
 typedef struct {
     esp_rmaker_ota_type_t type;
